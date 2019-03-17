34501b9c7cd437448ed491384085c610fe568add
zyfrank <zy_frank@hotmail.com>
2019-03-18 00:10:29 +0800

    add ipfs-s3c-storj-plugin support

--- profile.go
+++ profile.go
@@ -97,6 +97,43 @@ Inverse profile of the test profile.`,
 			return nil
 		},
 	},
+	"s3c-storjds": {
+		Description: "Replaces default datastore config with experimental storj",
+		Transform: func(c *Config) error {
+			c.Datastore.Spec = map[string]interface{}{
+				"type": "mount",
+				"mounts": []interface{}{
+					map[string]interface{}{
+						"mountpoint": "/blocks",
+						"name":       "s3ds",
+						"type":       "log",
+						"child": map[string]interface{}{
+							"accessKey":     os.Getenv("STORJ_ACCESS_KEY"),
+							"secretKey":     os.Getenv("STORJ_SECRET_KEY"),
+							"bucket":        "ipfs",
+							"region":        "us-east-1",
+							"endpoint":      "http://127.0.0.1:9000",
+							"rootDirectory": "",
+							"type":          "s3ds",
+							"logPath":       "",
+						},
+					},
+					map[string]interface{}{
+						"child": map[string]interface{}{
+							"compression": "none",
+							"path":        "datastore",
+							"type":        "levelds",
+						},
+						"mountpoint": "/",
+						"prefix":     "leveldb.datastore",
+						"type":       "measure",
+					},
+				},
+			}
+			return nil
+		},
+	},
+
 	"badgerds": {
 		Description: `Replaces default datastore configuration with experimental
 badger datastore.
